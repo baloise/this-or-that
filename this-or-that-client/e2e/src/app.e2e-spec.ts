2682ff4acb9bedafa5e35be024c54442068f5827
Philipp Steiner <philipp.steiner@baloise.ch>
2019-06-13 14:42:31 +0200

    clean angularCli and run config for it

--- this-or-that-client/e2e/src/app.e2e-spec.ts
+++ this-or-that-client/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to my-dream-app!');
+    expect(page.getTitleText()).toEqual('Welcome to this-or-that-client!');
   });
 
   afterEach(async () => {
