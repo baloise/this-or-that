d44fc038b84da04c1fc1a0bb28a3d63514eeb962
Yannick Holzenkamp <yannick@holzenkamp.me>
2019-10-07 17:34:54 +0200

    #27 Add some awesome UI features

--- app/lib/vote.dart
+++ app/lib/vote.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class VoteScreen extends StatelessWidget {
+class VoteScreen extends StatefulWidget {
+  VoteScreen({Key key, @required this.surveyCode}) : super(key: key);
 
   final String surveyCode;
 
-  VoteScreen({Key key, @required this.surveyCode}) : super(key: key);
+  @override
+  VoteScreenState createState() => VoteScreenState(surveyCode: surveyCode);
+}
+
+class VoteScreenState extends State<VoteScreen> {
+
+  final String surveyCode;
+  bool loading = false;
+
+  VoteScreenState({Key key, @required this.surveyCode}) : super();
+
+  @override
+  void initState() {
+    this.loading = true;
+    http.get("http://api.holzenkamp.me/spring-test/slow").then((response) {
+      setState(() => this.loading = false);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Second Screen"),
+        title: Text("Survey"),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back: ' + surveyCode),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Visibility(
+                visible: loading,
+                child: CircularProgressIndicator()
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Survey-ID: " + surveyCode),
+            )
+          ],
         ),
       ),
     );
