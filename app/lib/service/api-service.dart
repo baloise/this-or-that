44ccc3e68ec19a992ae2264d1eb58b87fcf5d33a
Yannick Holzenkamp <yannick@holzenkamp.me>
2019-10-08 15:32:09 +0200

    #27 Add some awesome UI features for the vote page

--- app/lib/service/api-service.dart
+++ app/lib/service/api-service.dart
@@ -8,20 +8,6 @@ const API_BASE_URL = "http://api.holzenkamp.me/spring-test";
 
 class ApiService {
 
-  /*
-  static DecisionSet fetchNewDecisionSet(String surveyId) {
-    String url = API_BASE_URL + "/" + surveyId + "/vote";
-    final response = http.get(url).then((response) {
-      print("=>" + response.body);
-      if (response.statusCode == 200) {
-        return DecisionSet.fromJson(json.decode(response.body));
-      } else {
-        throw Exception("Error"); // FIXME
-      }
-    });
-  }
-  */
-
   static Future<DecisionSet> fetchNewDecisionSet(String surveyId) async {
     String url = API_BASE_URL + "/" + surveyId + "/vote";
     final response = await http.get(url);
@@ -31,7 +17,7 @@ class ApiService {
     if (response.statusCode == 200) {
       return DecisionSet.fromJson(json.decode(response.body));
     } else {
-      throw Exception("Error"); // FIXME
+      throw Exception("Error");
     }
   }
 
@@ -42,7 +28,7 @@ class ApiService {
 
     return http.post(url, body: body).then((http.Response response) {
       if (response.statusCode != 200) {
-        throw new Exception("Error"); // FIXME
+        throw new Exception("Error");
       }
     });
   }
