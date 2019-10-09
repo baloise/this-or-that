07da4d47ea9b97aeef86b2b0fc39baa0e81a68c7
Markus Tiede <markus.tiede@baloise.ch>
2019-10-09 11:20:26 +0000

    #58 Rewrite backend to spring

--- server/src/main/java/com/baloise/open/thisorthat/api/ApiController.java
+++ server/src/main/java/com/baloise/open/thisorthat/api/ApiController.java
@@ -1,35 +1,40 @@
-package com.baloise.open.thisorthat;
+package com.baloise.open.thisorthat.api;
 
+import com.baloise.open.thisorthat.api.dto.Score;
+import com.baloise.open.thisorthat.api.dto.ScoreResponse;
+import com.baloise.open.thisorthat.api.dto.VoteRequest;
+import com.baloise.open.thisorthat.api.dto.VoteResponse;
+import com.baloise.open.thisorthat.service.SurveyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
 
 import java.util.Arrays;
-import java.util.concurrent.ThreadLocalRandom;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class ApiController {
 
+    private SurveyService surveyService = new SurveyService();
+
+    private String getUserId() {
+        String sessionId = RequestContextHolder.currentRequestAttributes().getSessionId();
+        return sessionId != null ? sessionId : "sessionIdUnavailable";
+    }
 
     @RequestMapping(value = "/{code}/vote", method = GET)
     @ResponseBody
-    public ResponseEntity<VoteResponse> getSurvey(@PathVariable("code") String surveyCode) throws InterruptedException {
-        Thread.sleep(1000);
-        if (surveyCode.equalsIgnoreCase("fail")) {
-            return new ResponseEntity<VoteResponse>(new VoteResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<VoteResponse> getSurvey(@PathVariable("code") String surveyCode) {
+        try {
+            VoteResponse voteResponse = surveyService.getVote(surveyCode, getUserId());
+            return new ResponseEntity<>(new VoteResponse(), HttpStatus.OK);
+        } catch (Throwable t) {
+            VoteResponse voteResponse = new VoteResponse();
+            voteResponse.setPerspective(t.getMessage());
+            return new ResponseEntity<>(voteResponse, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        int randomNum1 = ThreadLocalRandom.current().nextInt(0, 5);
-        int randomNum2 = ThreadLocalRandom.current().nextInt(0, 5);
-
-        VoteResponse vr = new VoteResponse();
-        vr.setId1(String.valueOf(randomNum1));
-        vr.setId2(String.valueOf(randomNum2));
-        vr.setPerspective("Something");
-        vr.setSurveyIsRunning(!surveyCode.equalsIgnoreCase("done"));
-        return new ResponseEntity<VoteResponse>(vr, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{code}/score", method = GET)
@@ -58,7 +63,7 @@ public class ApiController {
     @GetMapping(value = "/{code}/image/{imageId}", produces = "image/jpeg")
     public ResponseEntity<byte[]> getSurvey(@PathVariable("code") String surveyCode, @PathVariable("imageId") String
             imageId) {
-        if (imageId.equalsIgnoreCase("0")) {
+        /*if (imageId.equalsIgnoreCase("0")) {
             return new ResponseEntity<byte[]>(javax.xml.bind.DatatypeConverter.parseBase64Binary(Img.img1), HttpStatus.OK);
         }
         if (imageId.equalsIgnoreCase("1")) {
@@ -68,6 +73,8 @@ public class ApiController {
             return new ResponseEntity<byte[]>(javax.xml.bind.DatatypeConverter.parseBase64Binary(Img.img3), HttpStatus.OK);
         }
         return new ResponseEntity<byte[]>(javax.xml.bind.DatatypeConverter.parseBase64Binary(Img.img4), HttpStatus.OK);
+        */
+        return null;
     }
 
 }
