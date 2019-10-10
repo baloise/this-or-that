fba6d65e5541169798c5bed03b0b04a4af83c968
hirsch <gery.hirschfeld@w3tec.ch>
2019-10-10 12:01:30 +0200

    Fix: Add form element for validation

--- app/lib/create/create.dart
+++ app/lib/create/create.dart
@@ -45,46 +45,49 @@ class CreateScreenState extends State<CreateScreen> {
         actions: <Widget>[],
       ),
       body: SafeArea(
-          child: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
-            child: TextFormField(
-                controller: txtId,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a survey title';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter survey title',
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-            child: MaterialButton(
-              height: 60,
-              onPressed: choose,
-              child: Text("Add images", style: TextStyle(fontSize: 20)),
-              color: Colors.blueAccent[700],
-              textColor: Colors.white,
-              splashColor: Colors.white,
-              minWidth: double.infinity,
+          child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
+              child: TextFormField(
+                  controller: txtId,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a survey title';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter survey title',
+                  )),
             ),
-          ),
-          Expanded(
-            child: GridView.count(
-              primary: false,
-              padding: const EdgeInsets.all(20),
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              crossAxisCount: 3,
-              children: _images == null ? [] : _images,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: MaterialButton(
+                height: 60,
+                onPressed: choose,
+                child: Text("Add images", style: TextStyle(fontSize: 20)),
+                color: Colors.blueAccent[700],
+                textColor: Colors.white,
+                splashColor: Colors.white,
+                minWidth: double.infinity,
+              ),
             ),
-          )
-        ],
+            Expanded(
+              child: GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                children: _images == null ? [] : _images,
+              ),
+            )
+          ],
+        ),
       )),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.tealAccent[400],
