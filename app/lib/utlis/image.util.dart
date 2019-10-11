fd24187a5d1a0983dce908aabe11061e6c2c8094
Yannick Holzenkamp <yannick@holzenkamp.me>
2019-10-11 10:15:29 +0200

    Fix url parsing of qr code

--- app/lib/utlis/image.util.dart
+++ app/lib/utlis/image.util.dart
@@ -6,7 +6,7 @@ import 'package:image/image.dart';
 class ImageUtil {
   static Future<String> convertImages(String code, File file) async {
     Image image = decodeImage(file.readAsBytesSync());
-    Image thumbnail = copyResize(image, height: 1024);
+    Image thumbnail = copyResize(image, height: 512);
     List<int> imageBytes = encodeJpg(thumbnail);
     String base64Image = base64Encode(imageBytes);
     return "data:image/jpg;base64," + base64Image;
