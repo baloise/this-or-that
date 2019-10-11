67ca2932ad02fdc9ba08251c47bfb8bbde9258d7
Rudolf Biczok <rcbiczok@gmail.com>
2019-10-11 09:50:46 +0200

    Remove console prints

--- web/src/app/views/CreateSurveyContainer.vue
+++ web/src/app/views/CreateSurveyContainer.vue
@@ -121,8 +121,6 @@ export default class CreateSurveyContainer extends Vue {
     this.isLoading = true;
     const createSurveyRequest: CreateSurveyRequest = new CreateSurveyRequest();
     createSurveyRequest.perspective = this.perspective;
-    console.log(this.perspective);
-    console.log(createSurveyRequest);
     try {
       const response = await createSurvey(createSurveyRequest);
       this.surveyCode = response.code;
