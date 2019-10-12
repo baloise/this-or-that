d7e3dbcb733fbc7e5b728d6806caa43fa52662b6
Rudolf Biczok <rcbiczok@gmail.com>
2019-10-12 14:21:33 +0200

    Import upload image from fas and display it propably

--- web/src/plugins/fontawesome.plugin.ts
+++ web/src/plugins/fontawesome.plugin.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import {library} from '@fortawesome/fontawesome-svg-core';
-import {faCoffee, faUser, faTrophy, faEnvelope, faLink, faExclamationCircle} from '@fortawesome/free-solid-svg-icons';
+import {faUpload, faUser, faTrophy, faEnvelope, faLink, faExclamationCircle} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
-library.add(faUser, faTrophy, faEnvelope, faLink, faExclamationCircle);
+library.add(faUpload, faUser, faTrophy, faEnvelope, faLink, faExclamationCircle);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
