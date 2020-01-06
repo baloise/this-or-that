d887acf58b3a5e6cb376b75ebb7585f2b9d72170
Philipp Steiner <philipp.steiner@baloise.ch>
2020-01-06 13:48:12 +0100

    cleanup of dependencies modified Dockerfile

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-exec java ${JAVA_OPTS} \
--Djava.security.egd=file:/dev/./urandom \
--XX:+UnlockExperimentalVMOptions \
--XX:+UseCGroupMemoryLimitForHeap \
--jar "/opt/app/app.jar" "$@"
+exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -jar "/opt/app/app.jar" "$@"
--- server/src/main/java/com/baloise/open/thisorthat/Application.java
+++ server/src/main/java/com/baloise/open/thisorthat/Application.java
@@ -17,11 +17,9 @@ package com.baloise.open.thisorthat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
